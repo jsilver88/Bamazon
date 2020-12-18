8aa90ff914a255246d15252f60c31abd97be266b
Jared Silver <jrsilver88@gmail.com>
2020-12-18 10:06:23 -0800

    added user model and user router with mongooose and mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Jared',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('12345', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('12345', 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: '1',
