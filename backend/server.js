0179d890e5d0de0b39f853bbca2047037252bb9c
Jared Silver <jrsilver88@gmail.com>
2020-12-18 12:57:25 -0800

    created backend api to authenticate user with jwt and dotenv to secure token. check user email if it exists and check password using bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bamazon', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
